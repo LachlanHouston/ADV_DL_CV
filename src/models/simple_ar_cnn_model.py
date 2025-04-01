35012db30a686745562e766bfdbd9e4c4d7b4e88
carlLGN <s214624@dtu.dk>
2025-04-01 18:01:28 +0200

    added loss functions

--- src/models/simple_ar_cnn_model.py
+++ src/models/simple_ar_cnn_model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import pdb
+
 
 class ConvBlock(nn.Module):
     """Basic convolutional block with batch normalization and ReLU activation."""
